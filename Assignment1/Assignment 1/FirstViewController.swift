a2c147fbf653314c75548c2e4d80338fea72a414
student2017USA <baruth@t-online.de>
2017-02-01 20:29:23 +0100

    Assignment1
    Assignment
    NB.  All work on this assignment is to be done separately.  If you have questions, ask them in the discussion channel.
    1.  (10 Points) Create a Tabbed View Project and rename First and SecondViewControllers to be Simulation and InstrumentationViewControllers, respectively.  Verify that project compiles and runs in the simulator  
    2.  (40 points) Remove the unwanted text from the SimulationController scene and replace with a UIView which:
    	•	has a light blue background color  (you pick  the shade of blue)
    	•	maintains a square aspect ratio
    	•	stays both vertically and horizontally centered in the container
    	•	fills 80% of the shorter of the vertical or horizontal axis in either orientation for all size classes, taking account of the tab at the bottom
    	•	has the name LifeGrid in the Scene pane
    	•	neither the status bar nor the tab bar should not be obscured in any way
    	•	Verify that the view behaves as specified on both an iPhone and iPad configuration

---
+++ Assignment1/Assignment 1/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Assignment 1
+//
+//  Created by Angela Baruth on 01/01/17.
+//  Copyright © 2017 Spring 2017. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
